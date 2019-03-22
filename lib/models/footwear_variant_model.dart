f9a7cfe031104338ab0c022d10d6be0d80d383d4
Walter Steur <stewartrule@gmail.com>
2019-03-22 16:07:57 +0100

    cleanup

--- lib/models/footwear_variant_model.dart
+++ lib/models/footwear_variant_model.dart
@@ -2,10 +2,6 @@ import 'package:flutter/foundation.dart' show required;
 import 'package:json_annotation/json_annotation.dart'
     show JsonSerializable, JsonKey;
 
-// import '../reducers/root_reducer.dart';
-// import '../models/hsl_model.dart';
-// import '../models/footwear_size_model.dart';
-
 part 'footwear_variant_model.g.dart';
 
 @JsonSerializable(nullable: false)
@@ -32,34 +28,4 @@ class FootwearVariantModel {
       _$FootwearVariantModelFromJson(json);
 
   Map<String, dynamic> toJson() => _$FootwearVariantModelToJson(this);
-
-  // FootwearSizeModel size(RootState state) {
-  //   return state.settings.footwearSizes[sizeId];
-  //   // return state.settings.footwearSizes.values
-  //   //     .firstWhere((size) => size.id == sizeId);
-  // }
-
-  // HslModel color(RootState state) {
-  //   return state.settings.colors[colorId];
-
-  //   // return state.settings.colors.values
-  //   //     .firstWhere((color) => color.id == colorId);
-  // }
 }
-
-// /// Selectors
-// class State {
-//   Map<int, FootwearModel> footwear;
-//   Map<int, HslModel> colors;
-//   // List<HslModel> colors;
-// }
-
-// FootwearModel footwear(State state, int footwearId) {
-//   return state.footwear[footwearId];
-//   // .firstWhere((model) => model.id == footwearId);
-// }
-
-// HslModel color(State state, int colorId) {
-//   return state.colors[colorId];
-//   // return state.colors.firstWhere((model) => model.id == colorId);
-// }
