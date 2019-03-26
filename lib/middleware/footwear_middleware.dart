040cc4b69717116f80cfb07945423a7655dc36ed
Walter Steur <stewartrule@gmail.com>
2019-03-26 19:57:39 +0100

    inject http client into middleware

--- lib/middleware/footwear_middleware.dart
+++ lib/middleware/footwear_middleware.dart
@@ -1,32 +1,34 @@
 import 'package:redux/redux.dart';
 
-import '../util/http.dart';
+import '../util/http_client.dart';
 
 import '../models/footwear_model.dart';
 import '../reducers/root_reducer.dart';
 import '../actions/footwear_actions.dart';
 
-void fetchFootwear(
-  Store<RootState> store,
-  FetchFootwearAction action,
-  NextDispatcher next,
-) {
-  next(action);
+List<Middleware<RootState>> createFootwearMiddleware(HttpClient client) {
+  void fetchFootwear(
+    Store<RootState> store,
+    FetchFootwearAction action,
+    NextDispatcher next,
+  ) {
+    next(action);
 
-  http.get('/footwear').then((res) {
-    final data = res.map(
-      (key, value) => MapEntry(
-            key,
-            FootwearModel.fromJson(value as Map<String, dynamic>),
-          ),
-    );
+    client.get('/footwear').then((res) {
+      final data = res.map(
+        (key, value) => MapEntry(
+              key,
+              FootwearModel.fromJson(value as Map<String, dynamic>),
+            ),
+      );
 
-    return store.dispatch(
-      FetchFootwearCompletedAction(data: data),
-    );
-  });
+      return store.dispatch(
+        FetchFootwearCompletedAction(data: data),
+      );
+    });
+  }
+
+  return [
+    TypedMiddleware<RootState, FetchFootwearAction>(fetchFootwear),
+  ];
 }
-
-List<Middleware<RootState>> footwearMiddleware = [
-  TypedMiddleware<RootState, FetchFootwearAction>(fetchFootwear),
-];
