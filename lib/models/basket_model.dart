f59c8b827c60c14e88c69a5a31743437b552e486
Walter Steur <stewartrule@gmail.com>
2019-04-02 23:46:07 +0200

    add shopping basket

---
+++ lib/models/basket_model.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/foundation.dart' show required;
+
+import './basket_item_model.dart';
+import './footwear_variant_model.dart';
+import './footwear_model.dart';
+
+class BasketModel {
+  final List<BasketItemModel> items;
+
+  BasketModel({
+    @required this.items,
+  });
+
+  bool hasFootwearWithId(int footwearId) {
+    return items.any(
+      (BasketItemModel basketItem) =>
+          basketItem.variant.footwearId == footwearId,
+    );
+  }
+
+  bool hasFootwearVariant(FootwearVariantModel variant) {
+    return items.any(
+      (BasketItemModel basketItem) => basketItem.variant.id == variant.id,
+    );
+  }
+
+  BasketModel removeFootwear(FootwearModel footwear) {
+    return copyWith(
+      items: items
+          .where(
+            (BasketItemModel basketItem) =>
+                basketItem.variant.footwearId != footwear.id,
+          )
+          .toList(),
+    );
+  }
+
+  BasketModel removeFootwearVariant(FootwearVariantModel variant) {
+    return copyWith(
+      items: items
+          .where(
+            (BasketItemModel basketItem) => basketItem.variant.id != variant.id,
+          )
+          .toList(),
+    );
+  }
+
+  BasketModel removeItem(BasketItemModel item) {
+    return copyWith(
+      items: items
+          .where(
+            (BasketItemModel basketItem) =>
+                basketItem.variant.id != item.variant.id,
+          )
+          .toList(),
+    );
+  }
+
+  BasketModel updateFootwear({
+    @required FootwearVariantModel variant,
+    int amount,
+  }) {
+    return copyWith(
+      items: items
+          .map(
+            (BasketItemModel basketItem) => basketItem.variant.id == variant.id
+                ? basketItem.copyWith(
+                    amount: amount,
+                  )
+                : basketItem,
+          )
+          .toList(),
+    );
+  }
+
+  BasketModel addFootwear({
+    @required FootwearVariantModel variant,
+    @required int amount,
+  }) {
+    return hasFootwearVariant(variant)
+        ? this
+        : copyWith(
+            items: items
+              ..add(
+                BasketItemModel(
+                  variant: variant,
+                  amount: amount,
+                ),
+              ),
+          );
+  }
+
+  BasketModel copyWith({
+    @required List<BasketItemModel> items,
+  }) {
+    return BasketModel(
+      items: items ?? this.items,
+    );
+  }
+}
---
+++ lib/reducers/basket_reducer.dart
@@ -0,0 +1,57 @@
+import 'package:redux/redux.dart';
+import '../actions/basket_actions.dart';
+import '../models/basket_model.dart';
+
+BasketModel _addFootwearVariantReducer(
+  BasketModel basket,
+  BasketAddFootwearAction action,
+) =>
+    basket.addFootwear(
+      variant: action.variant,
+      amount: action.amount,
+    );
+
+BasketModel _updateFootwearVariantReducer(
+  BasketModel basket,
+  BasketUpdateFootwearAction action,
+) =>
+    basket.updateFootwear(
+      variant: action.variant,
+      amount: action.amount,
+    );
+
+BasketModel _removeFootwearReducer(
+  BasketModel basket,
+  BasketRemoveFootwearAction action,
+) =>
+    basket.removeFootwear(action.footwear);
+
+BasketModel _removeFootwearVariantReducer(
+  BasketModel basket,
+  BasketRemoveFootwearVariantAction action,
+) =>
+    basket.removeFootwearVariant(action.variant);
+
+BasketModel _removeItemReducer(
+  BasketModel basket,
+  BasketRemoveItemAction action,
+) =>
+    basket.removeItem(action.item);
+
+Reducer<BasketModel> basketReducer = combineReducers([
+  TypedReducer<BasketModel, BasketAddFootwearAction>(
+    _addFootwearVariantReducer,
+  ),
+  TypedReducer<BasketModel, BasketUpdateFootwearAction>(
+    _updateFootwearVariantReducer,
+  ),
+  TypedReducer<BasketModel, BasketRemoveFootwearVariantAction>(
+    _removeFootwearVariantReducer,
+  ),
+  TypedReducer<BasketModel, BasketRemoveFootwearAction>(
+    _removeFootwearReducer,
+  ),
+  TypedReducer<BasketModel, BasketRemoveItemAction>(
+    _removeItemReducer,
+  ),
+]);
